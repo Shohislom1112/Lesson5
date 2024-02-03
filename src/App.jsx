6a7967e5bf96eaeac938dee5bda4585fd1792bb1
Shohislom1112 <ravshanovshohislom11@gmail.com>
2024-02-03 15:30:52 +0500

    SSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { Browser as Router, Routes, Route } from "react-router-dom";
-import { Home } from "./pages";
+import Home from "./pages/Home";
+import Catalog from "./pages/Catalog"
+import NotFound from "./pages/NotFound";
+import Onas from "./pages/Onas";
+import Login from "./pages/Login";
+// import { Home } from "./pages";
 
 const App = () => {
   return (
     <div>
-      <Router></Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>} />
+          <Route path="/catalog" element={<Catalog/>} />
+          <Route path="*" element={<NotFound/>} />
+          <Route path="/onas" element={<Onas/>} />
+            
+        </Routes>
+      </Router>
     </div>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return <Header className="Header">
+    <NavLink to="/"> <img src="" alt="" /></NavLink>
+    <NavLink></NavLink>
+    <NavLink></NavLink>
+    <NavLink></NavLink>
+    <NavLink></NavLink>
+    <NavLink></NavLink>
+  </Header>;
 };
 
 export default Header;
